87fe8bc4d5a60ff74bc188f8e5428065246a50ce
NicoEby <nicolas.eppler@icr.ch>
2019-09-05 07:44:41 +0200

    Updates

---
+++ precache-manifest.9014a87d154875d7db699775c7583718.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d98f4043dbe609ecbf5ef20727caf64",
+    "url": "/EPFL-course-project/index.html"
+  },
+  {
+    "revision": "b52933e449f762224270",
+    "url": "/EPFL-course-project/static/css/main.3d1a965e.chunk.css"
+  },
+  {
+    "revision": "f40c20ae7f462a0397e8",
+    "url": "/EPFL-course-project/static/js/2.88c5d3ed.chunk.js"
+  },
+  {
+    "revision": "b52933e449f762224270",
+    "url": "/EPFL-course-project/static/js/main.4f9fae69.chunk.js"
+  },
+  {
+    "revision": "820ec51f8ecf0eaa3a5f",
+    "url": "/EPFL-course-project/static/js/runtime~main.87cb100a.js"
+  }
+]);
